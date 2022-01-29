40d78dd30de61db27de0d0cd1fe4ff3bd1ebfff0
Simon Post <postse@live.unc.edu>
2022-01-29 18:39:39 -0500

    telegun is now held and has a trail

--- Assets/Scripts/AbilityScripts/Telegun.cs
+++ Assets/Scripts/AbilityScripts/Telegun.cs
@@ -6,27 +6,27 @@ public class Telegun : Ability
     public GameObject bullet;
     public float bulletSpeed;
     public float startingDistance;
-    private bool inUse = false;
+    private bool inUse;
     private GameObject currentBullet;
+
+    private void Start()
+    {
+        inUse = false;
+        currentBullet = null;
+        // playerObject = GameObject.Find("Player");
+    }
 
     public override void use()
     {
-        GameObject playerObject = GameObject.Find("Player");
         if (!inUse)
         {
+            GameObject playerObject = GameObject.Find("Player");
             var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var normalizedDirection = new Vector2(mousePosition.x - playerObject.transform.position.x, mousePosition.y - playerObject.transform.position.y).normalized;
             currentBullet = Instantiate(bullet, new Vector2(playerObject.transform.position.x, playerObject.transform.position.y) + (normalizedDirection * startingDistance), transform.rotation);
             currentBullet.GetComponent<Rigidbody2D>().velocity = normalizedDirection * bulletSpeed;
             inUse = true;
         }
-        else
-        {
-            playerObject.transform.position = currentBullet.transform.position;
-            Destroy(currentBullet);
-            inUse = false;
-        }
-
     }
 
     public override bool canUse()
@@ -36,6 +36,12 @@ public class Telegun : Ability
 
     public override void notUsing()
     {
-
+        if (inUse)
+        {
+            GameObject playerObject = GameObject.Find("Player");
+            playerObject.transform.position = currentBullet.transform.position;
+            Destroy(currentBullet);
+            inUse = false;
+        }
     }
 }
