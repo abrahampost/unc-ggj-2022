f1d0b530c01c27bbb7c077804f37ac91ba5164b5
Denver Massey <denmas22@live.unc.edu>
2022-01-29 16:11:47 -0500

    added plug knight

--- Assets/Scripts/EnemyMovement/PlugKnightMovement.cs
+++ Assets/Scripts/EnemyMovement/PlugKnightMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkeeterMovement : MonoBehaviour
+public class PlugKnightMovement : MonoBehaviour
 {
     public float speed;
     public float maxSpeed;
