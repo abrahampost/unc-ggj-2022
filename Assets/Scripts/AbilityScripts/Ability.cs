e1d9c7a3f989acf92d0df1863f2fd2ca32d545ad
Simon Post <postse@live.unc.edu>
2022-01-29 01:52:06 -0500

    Added ability usage, bullet, and telebullet

---
+++ Assets/Scripts/AbilityScripts/Ability.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+public abstract class Ability : MonoBehaviour
+{
+    public abstract bool canUse();
+    public abstract void use();
+}
---
+++ Assets/Scripts/AbilityScripts/Gun.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Gun : Ability  {
+
+    public GameObject bullet;
+    public float bulletSpeed;
+    public float timeAlive;
+    public float startingDistance;
+    
+    public override void use()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GameObject playerObject = GameObject.Find("Player");
+        var normalizedDirection = new Vector2(mousePosition.x - playerObject.transform.position.x, mousePosition.y - playerObject.transform.position.y).normalized;
+        GameObject newBullet = Instantiate(bullet, new Vector2(playerObject.transform.position.x, playerObject.transform.position.y) + (normalizedDirection*startingDistance), transform.rotation);
+        newBullet.GetComponent<Rigidbody2D>().velocity = normalizedDirection * bulletSpeed;
+        Destroy (newBullet, timeAlive);
+    }
+
+    public override bool canUse()
+    {
+        return true;
+    }
+}
