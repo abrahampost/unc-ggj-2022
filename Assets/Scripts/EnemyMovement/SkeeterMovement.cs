a2a272efa3452a026cbfa7cd445d386f51730d0a
Denver Massey <denmas22@live.unc.edu>
2022-01-29 15:50:02 -0500

    updated skeeter movement

--- Assets/Scripts/EnemyMovement/SkeeterMovement.cs
+++ Assets/Scripts/EnemyMovement/SkeeterMovement.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class SkeeterMovement : MonoBehaviour
 {
     public float speed;
+    public float maxSpeed;
+    public float decel;
     public GameObject target;
+    public float yOffset;
 
     // Start is called before the first frame update
     void FixedUpdate()
     {
 
         // Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Get Target position
         Vector2 targetPosition = target.GetComponent<Rigidbody2D>().position;
-        Vector2 targetVector = (targetPosition - gameObject.GetComponent<Rigidbody2D>().position).normalized;
+        targetPosition.y += yOffset;
+
+        Vector2 targetVector = (targetPosition - gameObject.GetComponent<Rigidbody2D>().position);
         Vector2 deltaVel = speed * targetVector;
 
-        gameObject.GetComponent<Rigidbody2D>().velocity += deltaVel * Time.deltaTime;
+        // Go toward target
+        // gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.ClampMagnitude(deltaVel * Time.deltaTime, maxSpeed);
+        Vector2 newVel = Vector2.ClampMagnitude(gameObject.GetComponent<Rigidbody2D>().velocity + deltaVel * Time.deltaTime, maxSpeed);
+
+        if (newVel.magnitude > .01) {
+            gameObject.GetComponent<Rigidbody2D>().velocity = newVel / decel;
+        } else {
+            gameObject.GetComponent<Rigidbody2D>().velocity = newVel;
+        }
         
+        
+        // Flip sprite
         if (targetVector.x < 0) {
             GetComponent<SpriteRenderer>().flipX = false;
         } else {
