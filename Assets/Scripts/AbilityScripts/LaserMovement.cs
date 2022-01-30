4b1052c844895b0573ec868760b170f0d721c1aa
Denver Massey <denmas22@live.unc.edu>
2022-01-30 09:59:20 -0500

    plasma punk shoots now

--- Assets/Scripts/AbilityScripts/LaserMovement.cs
+++ Assets/Scripts/AbilityScripts/LaserMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class LaserMovement : MonoBehaviour
 {
     public Animator animator;
+    public float explosionTime;
 
     public IEnumerator SetHit() {
         animator.SetBool("Hit", true);
         GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 
-        yield return new WaitForSeconds(0.4f);
-        Destroy(this.gameObject);
+        yield return new WaitForSeconds(explosionTime);
+        if (this.gameObject != null) {
+            Destroy(this.gameObject);
+        }
     }
 }
