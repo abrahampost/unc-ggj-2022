bb9813475b61d2bb9608f7f9c89c2b4a41117c36
LoganQualk <loganq@gmail.com>
2022-01-30 02:18:48 -0500

    Almost finished cutscene and added levels 1 to 4

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -26,4 +26,20 @@ public class MainMenu : MonoBehaviour
     public void Quit() {
         Debug.Log("Quit");
     }
+
+    public void Level1() {
+        SceneManager.LoadScene("Level01");
+    }
+
+    public void Level2() {
+        SceneManager.LoadScene("Level02");
+    }
+
+    public void Level3() {
+        SceneManager.LoadScene("Level03");
+    }
+
+    public void Level4() {
+        SceneManager.LoadScene("Level04");
+    }
 }
