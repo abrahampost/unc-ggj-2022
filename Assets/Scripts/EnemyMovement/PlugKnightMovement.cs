0295e1b8120a94b69f820ff6cc5afbe6b2fd625e
Denver Massey <denmas22@live.unc.edu>
2022-01-29 19:46:28 -0500

    Plug knight is gonna kill a motherfucker

--- Assets/Scripts/EnemyMovement/PlugKnightMovement.cs
+++ Assets/Scripts/EnemyMovement/PlugKnightMovement.cs
@@ -9,8 +9,26 @@ public class PlugKnightMovement : MonoBehaviour
     public float decel;
     public GameObject target;
     public float yOffset;
+    public Animator animator;
+    public float readyTime;
+    public float dashTime;
+    public float pauseTime;
+    public float distanceToDash;
+    private bool collided = false;
 
-    // Start is called before the first frame update
+    private enum State {
+        WALKING = 0,
+        READYING = 1,
+        DASHING = 2,
+        PAUSING = 3
+    }
+
+    void Update()
+    {
+        // GetComponent<Rigidbody2D>()
+        
+    }
+
     void FixedUpdate()
     {
 
@@ -19,7 +37,7 @@ public class PlugKnightMovement : MonoBehaviour
         Vector2 targetPosition = target.GetComponent<Rigidbody2D>().position;
         targetPosition.y += yOffset;
 
-        Vector2 targetVector = (targetPosition - gameObject.GetComponent<Rigidbody2D>().position);
+        Vector2 targetVector = targetPosition - gameObject.GetComponent<Rigidbody2D>().position;
         Vector2 deltaVel = speed * targetVector;
         deltaVel.y = 0;
 
@@ -27,18 +45,67 @@ public class PlugKnightMovement : MonoBehaviour
         // gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.ClampMagnitude(deltaVel * Time.deltaTime, maxSpeed);
         Vector2 newVel = Vector2.ClampMagnitude(gameObject.GetComponent<Rigidbody2D>().velocity + deltaVel * Time.deltaTime, maxSpeed);
 
-        if (newVel.magnitude > .01) {
-            gameObject.GetComponent<Rigidbody2D>().velocity = newVel / decel;
-        } else {
-            gameObject.GetComponent<Rigidbody2D>().velocity = newVel;
+        // Normal walking
+        if (animator.GetInteger("State") == ((int)State.WALKING)) {
+            if (newVel.magnitude > .01) {
+                gameObject.GetComponent<Rigidbody2D>().velocity = newVel / decel;
+            } else {
+                gameObject.GetComponent<Rigidbody2D>().velocity = newVel;
+            }
+        }
+
+        // print(targetVector.magnitude);
+        // Ready dash attack
+        if (targetVector.magnitude < distanceToDash && animator.GetInteger("State") == ((int)State.WALKING)) {
+            StartCoroutine(dashAttack());
+            animator.SetInteger("State", ((int)State.READYING));
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         }
         
+        // Dash Attack
+        if (animator.GetInteger("State") == ((int)State.DASHING)) {
+            newVel *= 3;
+            if (newVel.magnitude > .01) {
+                gameObject.GetComponent<Rigidbody2D>().velocity = newVel / decel;
+            } else {
+                gameObject.GetComponent<Rigidbody2D>().velocity = newVel;
+            }
+        }
         
+        // Pause between attacks
+        if (animator.GetInteger("State") == ((int)State.PAUSING)) {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+
         // Flip sprite
         if (targetVector.x < 0) {
             transform.localScale = new Vector3(1, 1, 1);
         } else {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    IEnumerator dashAttack() {
+        yield return new WaitForSeconds(readyTime);
+        animator.SetInteger("State", ((int)State.DASHING));
+
+        yield return new WaitForSeconds(dashTime);
+        if (!collided) {
+            animator.SetInteger("State", ((int)State.PAUSING));
+            yield return new WaitForSeconds(pauseTime);
+            animator.SetInteger("State", ((int)State.WALKING));
+            collided = false;
+        }
+
+    }
+
+    IEnumerator OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            animator.SetInteger("State", ((int)State.PAUSING));
+            collided = true;
+
+            yield return new WaitForSeconds(pauseTime);
+            animator.SetInteger("State", ((int)State.WALKING));
         }
     }
 }
