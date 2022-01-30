09190007e5c7c9105a620a81b5978ca383301d65
Abraham Post <abrahampost@yahoo.com>
2022-01-30 00:11:24 -0500

    Fix bullets for fixed camera

--- Assets/Scripts/AbilityScripts/Gun.cs
+++ Assets/Scripts/AbilityScripts/Gun.cs
@@ -13,10 +13,10 @@ public class Gun : Ability
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = Camera.main.nearClipPlane;
         var mousePosition = Camera.main.ScreenToWorldPoint(mousePos);
-        print(mousePosition.x + " " + mousePosition.y + " " + mousePosition.z);
 
         GameObject playerObject = GameObject.Find("Player");
         var normalizedDirection = new Vector2(mousePosition.x - playerObject.transform.position.x, mousePosition.y - playerObject.transform.position.y).normalized;
+        Debug.Log(normalizedDirection);
         GameObject newBullet = Instantiate(bullet, new Vector2(playerObject.transform.position.x, playerObject.transform.position.y) + (normalizedDirection * startingDistance), Quaternion.AngleAxis(Mathf.Atan2(normalizedDirection.y, normalizedDirection.x) * Mathf.Rad2Deg, Vector3.forward));
         newBullet.GetComponent<Rigidbody2D>().velocity = normalizedDirection * bulletSpeed;
         Destroy(newBullet, timeAlive);
